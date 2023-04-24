9aa2cb27f0c685eb98e6dc0b0dd5a8001cb1c58f
EB <e.bains@hotmail.co.uk>
2023-04-24 17:59:52 +0100

    Added Yelp API info.

--- function.js
+++ function.js
@@ -1,5 +1,13 @@
 // 1st API Key- Yelp
 
+// API key
+
+var APIKey "RX7BAzW_jDhKGt3Lhr4wEzARJiYNehKSWcdyiN15hZDOIStKUF6EBuQzuQ7bnZSFHknxHq0XIMzmEVIiF7SDSQXiecDiH5mQIAw3yT_1VPxiXTuLzse6iZRng4BBZHYx";
+
+
+// URL query to data base
+
+var queryURL= "https://api.yelp.com/v3/businesses/search"
 
 
 
