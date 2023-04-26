1af5b5e97bc36b8781417136e6431fe14292b779
EB <e.bains@hotmail.co.uk>
2023-04-26 21:04:24 +0100

    Added second javascript file, so the inspire api doesn't interfer with the timer.

--- function.js
+++ function.js
@@ -1,67 +1,22 @@
-
 //1st API key - inspire button
 
+var APIkey ="pLXZdmdLnjuXpszmEnRtOJh6iVevOQD67K9PZO0AvFz89g1vPWfqnr4o"
+
+var queryURL = "https://api.pexels.com/v1/search"
+
+
+//end point https://api.pexels.com/v1/
 
 
 
+//Ajax GET request to our URl
+$.ajax({
+
+url;queryURL,
 
 
-// 2nd API Key- Maps- in HTML doc
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Page views- local storage
-
-
-var count= document.getElementById("count");
-
-//function
-pageVisits();
-
-//function defined
-function pageVisits(){
-
-//using 'let' value as we want the value to chnage as the number of visits increase- const is for fixed values?
-let visits;
-
-//  set to 'one' as we have already visited the page- open through the inspector tool-> local storage 
-
-if (!localStorage.getItem("visits")) localStorage.setItem ("visits",1);
-
-//if local storage value doesn't already exisit?
-else {
-
-    //'+' changes from string(text) to number value
-visits =+localStorage.getItem("visits");
-
-var visitedCount=visits +1;
-
-//adds the number of visits to local storage
-localStorage.setItem("visits",visitedCount)
-}
-
-count.innerText=localStorage.getItem("visits");
 
 
 }
 
-
-//element id check
-
-console.log(count);
-
+)
---
+++ visit.js
@@ -0,0 +1,42 @@
+
+
+//Page views- local storage
+
+
+var count= document.getElementById("count");
+
+//function
+pageVisits();
+
+//function defined
+function pageVisits(){
+
+//using 'let' value as we want the value to chnage as the number of visits increase- const is for fixed values?
+let visits;
+
+//  set to 'one' as we have already visited the page- open through the inspector tool-> local storage 
+
+if (!localStorage.getItem("visits")) localStorage.setItem ("visits",1);
+
+//if local storage value doesn't already exisit?
+else {
+
+    //'+' changes from string(text) to number value
+visits =+localStorage.getItem("visits");
+
+var visitedCount=visits +1;
+
+//adds the number of visits to local storage
+localStorage.setItem("visits",visitedCount)
+}
+
+count.innerText=localStorage.getItem("visits");
+
+
+}
+
+
+//element id check
+
+console.log(count);
+
