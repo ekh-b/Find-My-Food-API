e7182d98870cb96b20ebffeb66558118a5fc9f2c
Bashi <bashiabduwali3@hotmail.com>
2023-04-27 15:38:09 +0100

    lniked the api and made a new currency file codes

---
+++ script.js
@@ -0,0 +1,3 @@
+var mainApiKey = "https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD"
+var fromCurrencyCon = document.getElementById("prev-select")
+var afterCurrencyCon = document.getElementById("after-select")
