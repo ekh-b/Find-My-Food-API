5ff17fd5435d70fef78b688ef21cf9193825db9c
Bashi <bashiabduwali3@hotmail.com>
2023-04-27 16:05:43 +0100

    created a dropdown list menu from array

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 var mainApiKey = "https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD"
 var fromCurrencyCon = document.getElementById("prev-select")
 var afterCurrencyCon = document.getElementById("after-select")
+
+//Dropdown list from currency.js
+
+fromCurrencyCon.forEach((currency) => {
+    var pickCurrency = document.createElement("pickCurrency");
+    pickCurrency.value = currency;
+    pickCurrency.text= currency;
+    fromCurrencyCon.add(pickCurrency)
+})
